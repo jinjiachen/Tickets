8d7e4fcd0ecf3f7548e394d5f1593ced3ccd082d
jinjiachen_tux@163.com <jinjiachen_tux@163.com>
2023-08-03 15:27:22 +0800

    模拟点击抢票

--- damai.py
+++ damai.py
@@ -1,7 +1,11 @@
 #coding=utf-8
 
 import pyautogui
+from pymouse import PyMouse
 
-for i in range(1,100):
-    pyautogui.click(1041,935,duration=0.01)
+m=PyMouse()
+for i in range(1,50):
+    print(i)
+#    pyautogui.click(1150,935,duration=0.0001)
+    m.click(1150,935,1)
 #    pyautogui.click(1041,935)
